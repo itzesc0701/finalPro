1e9d9c236fadb5567ada87f109e8663031f7f5d2
itzesc0701 <itzelcordeo2007@gmail.com>
2023-06-29 09:02:19 -0600

    Se implemento la clase producto model codigo

--- src/main/java/com/mycompany/finalp/model/ProductoModel.java
+++ src/main/java/com/mycompany/finalp/model/ProductoModel.java
@@ -9,5 +9,59 @@ package com.mycompany.finalp.model;
  * @author LENOVO
  */
 public class ProductoModel {
+     private String nombre;
+     private Integer cantidad;
+     private String descripcion;
+     private Double precio;
+
+    public ProductoModel() {
+    }
+
+    public ProductoModel(String nombre, Integer cantidad, String descripcion, Double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoModel{" + "nombre=" + nombre + ", cantidad=" + cantidad + ", descripcion=" + descripcion + ", precio=" + precio + '}';
+    }
     
+
+    
+     
 }
